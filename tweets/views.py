d6ec6c5f9cf31f88ffede50200bf042d934a2e7d
awesomeo184 <awesomeo184@gmail.com>
2020-08-26 18:17:53 +0900

    [1]ADD templates, home_view, detail_tweet_view

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,22 @@
+from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render
+from .models import Tweet
 
-# Create your views here.
+
+def home_view(request):
+    return HttpResponse("<h1>Hello World</h1>")
+
+
+def tweet_detail_view(request, tweet_id):
+    data = {
+        'id': tweet_id,
+    }
+    status = 200
+    try:
+        tweet = Tweet.objects.get(id=tweet_id)
+        data['content'] = tweet.content
+    except:
+        data['message'] = "Not Found"
+        status = 404
+
+    return JsonResponse(data, status=status)
