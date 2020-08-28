c39648608447811a62b817487ab9409055b5d579
awesomeo184 <awesomeo184@gmail.com>
2020-08-28 19:07:44 +0900

    [21]CHANGE pure django views to DRF views

--- tweets/views.py
+++ tweets/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse, Http404, JsonResponse
 from django.shortcuts import render, redirect
 from django.utils.http import is_safe_url
 from django.conf import settings
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Tweet
 from .forms import TweetForm
 from .serializers import TweetSerializer
@@ -13,12 +15,13 @@ def home_view(request, *args, **kwargs):
     return render(request, 'page/home.html', context={})
 
 
+@api_view(['POST'])
 def tweet_create_view(request, *args, **kwargs):
-    serializer = TweetSerializer(data=request.POST or None)
-    if serializer.is_valid():
-        obj = serializer.save(user=request.user)
-        return JsonResponse(serializer.data, status=201)
-    return JsonResponse({}, status=400)
+    serializer = TweetSerializer(data=request.POST)
+    if serializer.is_valid(raise_exception=True):
+        serializer.save(user=request.user)
+        return Response(serializer.data, status=201)
+    return Response({}, status=400)
 
 
 def tweet_create_view_pure_django(request, *args, **kwargs):
@@ -35,7 +38,7 @@ def tweet_create_view_pure_django(request, *args, **kwargs):
         obj.user = user
         obj.save()
         if request.is_ajax():
-            return JsonResponse(obj. serialize(), status=201)
+            return JsonResponse(obj.serialize(), status=201)
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
@@ -45,7 +48,14 @@ def tweet_create_view_pure_django(request, *args, **kwargs):
     return render(request, 'components/form.html', context={'form': form})
 
 
+@api_view(['GET'])
 def tweet_list_view(request, *args, **kwargs):
+    query_set = Tweet.objects.all()
+    serializer = TweetSerializer(query_set, many=True)
+    return Response(serializer.data)
+
+
+def tweet_list_view_pure_django(request, *args, **kwargs):
     query_set = Tweet.objects.all()
     tweet_list = [tweet.serialize() for tweet in query_set]
     data = {
@@ -54,7 +64,14 @@ def tweet_list_view(request, *args, **kwargs):
     return JsonResponse(data)
 
 
+@api_view(['GET'])
 def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    tweet = Tweet.objects.filter(id=tweet_id)
+    serializer = TweetSerializer(tweet)
+    return Response(serializer.data)
+
+
+def tweet_detail_view_pure_django(request, tweet_id, *args, **kwargs):
     data = {
         'id': tweet_id,
     }
